36602ae444e18f2cd78dee2abb2fe152aba2ba56
geekpradd <190050089@iitb.ac.in>
2021-07-12 01:13:16 +0530

    finish core protocol, add tests

--- libtriptych/src/signature/triptych.rs
+++ libtriptych/src/signature/triptych.rs
@@ -8,28 +8,7 @@ use crate::util;
 use crate::Errors::{self, TriptychError};
 use std::convert::TryInto;
 use sha2::Sha512;
-use std::io::Error;
 
-pub fn pad(n: &usize, m: &usize) -> Vec<usize> {
-    let mut s = format!("{:b}", n); // this is using base 2, for other bases this will change
-    
-    if s.chars().count() < *m{
-        s = format!("{}{}", "0".repeat(*m - s.chars().count()), s);
-    }
-
-    return s.chars().rev().map(|i| i as usize - '0' as usize).collect::<Vec<usize>>();
-}
-
-pub fn convolve(x: &[Scalar], y: &[Scalar]) -> Vec<Scalar> {
-    let mut r: Vec<Scalar> = vec![Scalar::zero(); x.len()+1];
-    for i in 0..x.len(){
-        for j in 0..y.len(){
-            r[i+j] += x[i]*y[i];
-        }
-    }
-
-    return r;
-}
 #[derive(Clone, Debug)]
 pub struct TriptychEllipticCurveState {
     J: RistrettoPoint,
@@ -56,19 +35,24 @@ pub struct Signature {
 }
 
 // Commitment to Zero Proof
-// SARANG docs fill in, need to add here later
-pub fn prove(M: &Vec<RistrettoPoint>, l: &usize, r: &Scalar, m: &usize) -> Signature{
+
+//  M: public key list
+// l: M_l = rG
+// r: Pedersen blinder for M[l]
+//  m: dimension such that len(M) == 2**m
+
+// This is the core Sigma Protocol being implemented, not the signature protocol
+pub fn base_prove(M: &[RistrettoPoint], l: &usize, r: &Scalar, m: &usize, message: &str) -> Signature{
     let n: usize = 2; // base of decomposition, Tryptich supports arbitary base, we prefer binary here
 
     // To-DO: RANDOM SEED NOT IMPLEMENTED YET, REFER SARANG'S REPO
-    let H = util::hash_to_point("H");
     let U = util::hash_to_point("U");
 
     let G = util::hash_to_point("G"); 
     // In Risretto Curve, all POINTS are generators. G choice is arbitary here
     let mut rng = rand::thread_rng();
 
-    let mut transcript: Vec<u8> = Vec::with_capacity(1000);
+    let mut transcript: Vec<u8> = Vec::with_capacity(40000);
 
     // Error Checks are left, need to add that 
     let J = r.invert()*U;
@@ -86,30 +70,32 @@ pub fn prove(M: &Vec<RistrettoPoint>, l: &usize, r: &Scalar, m: &usize) -> Signa
         });
     }
 
-    let mut A = util::pedersen_commitment(&a, &rA);
+    let A = util::pedersen_commitment(&a, &rA);
 
     for entry in M {
         transcript.extend_from_slice(entry.compress().as_bytes());
     }
+
+    transcript.extend_from_slice(message.as_bytes());
     transcript.extend_from_slice(J.compress().as_bytes());
     transcript.extend_from_slice(A.compress().as_bytes());
     
 
 
-    let mut s = pad(&l, &m); // this is for base 2
+    let s = util::pad(&l, &m); // this is for base 2
 
-    let mut b = (0..*m).map(|j| (0..n).map(|i| util::delta(&s[j], &i)).collect::<Vec<Scalar>>()).collect::<Vec<Vec<Scalar>>>();
+    let b = (0..*m).map(|j| (0..n).map(|i| util::delta(&s[j], &i)).collect::<Vec<Scalar>>()).collect::<Vec<Vec<Scalar>>>();
 
-    let mut B = util::pedersen_commitment(&b, &rB);
+    let B = util::pedersen_commitment(&b, &rB);
 
-    let mut c = (0..*m).map(|j| (0..n).map(|i| a[j][i]*(Scalar::one() - b[j][i] - b[j][i])).collect::<Vec<Scalar>>()).collect::<Vec<Vec<Scalar>>>();
+    let c = (0..*m).map(|j| (0..n).map(|i| a[j][i]*(Scalar::one() - b[j][i] - b[j][i])).collect::<Vec<Scalar>>()).collect::<Vec<Vec<Scalar>>>();
 
-    let mut C = util::pedersen_commitment(&c, &rC);
+    let C = util::pedersen_commitment(&c, &rC);
 
     // the minus may not work here, check later
-    let mut d = (0..*m).map(|j| (0..n).map(|i| -a[j][i]*a[j][i]).collect::<Vec<Scalar>>()).collect::<Vec<Vec<Scalar>>>();
+    let d = (0..*m).map(|j| (0..n).map(|i| -a[j][i]*a[j][i]).collect::<Vec<Scalar>>()).collect::<Vec<Vec<Scalar>>>();
 
-    let mut D = util::pedersen_commitment(&d, &rD);
+    let D = util::pedersen_commitment(&d, &rD);
 
     transcript.extend_from_slice(B.compress().as_bytes());
     transcript.extend_from_slice(C.compress().as_bytes());
@@ -122,21 +108,21 @@ pub fn prove(M: &Vec<RistrettoPoint>, l: &usize, r: &Scalar, m: &usize) -> Signa
     let mut p = (0..N).map(|_| vec![]).collect::<Vec<Vec<Scalar>>>();
 
     for k in 0..N {
-        let binary_k = pad(&k, &m); // This can be sped up by gray codes, will have to change after bench
+        let binary_k = util::pad(&k, &m); // This can be sped up by gray codes, will have to change after bench
         p[k] = vec![a[0][binary_k[0]], util::delta(&s[0], &binary_k[0])];
 
         for j in 1..*m {
-            p[k] = convolve(&p[k], &vec![a[j][binary_k[j]], util::delta(&s[j], &binary_k[j])]);
+            p[k] = util::convolve(&p[k], &vec![a[j][binary_k[j]], util::delta(&s[j], &binary_k[j])]);
         }
     }
 
     
 
-    let mut rho = (0..*m).map(|_| Scalar::random(&mut rng)).collect::<Vec<Scalar>>();
+    let rho = (0..*m).map(|_| Scalar::random(&mut rng)).collect::<Vec<Scalar>>();
 
-    let mut Y = (0..*m).map(|i| rho[i]*J).collect::<Vec<RistrettoPoint>>();
+    let Y = (0..*m).map(|i| rho[i]*J).collect::<Vec<RistrettoPoint>>();
 
-    let mut X = (0..*m).map(|j| (0..N).fold(rho[j]*G, |acc, k|{
+    let X = (0..*m).map(|j| (0..N).fold(rho[j]*G, |acc, k|{
                                             acc + p[k][j]*M[k]
                                         })).collect::<Vec<RistrettoPoint>>();
 
@@ -151,14 +137,14 @@ pub fn prove(M: &Vec<RistrettoPoint>, l: &usize, r: &Scalar, m: &usize) -> Signa
     //  need to hash here and then output can be created (scalar state)
     let challenge = Scalar::hash_from_bytes::<Sha512>(&transcript);
 
-    let mut f = (0..*m).map(|j| (1..n).map(|i| util::delta(&s[j], &i)*challenge + a[j][i]).collect::<Vec<Scalar>>()).collect::<Vec<Vec<Scalar>>>();
+    let f = (0..*m).map(|j| (1..n).map(|i| util::delta(&s[j], &i)*challenge + a[j][i]).collect::<Vec<Scalar>>()).collect::<Vec<Vec<Scalar>>>();
 
-    let mut zA = rA + challenge*rB;
-    let mut zC = challenge*rC + rD;
+    let zA = rA + challenge*rB;
+    let zC = challenge*rC + rD;
 
     
 
-    let mut z = r*util::power(&challenge, &m) - (0..*m).fold(Scalar::zero(), |acc, j|{ acc + rho[j]*util::power(&challenge, &j)});
+    let z = r*util::power(&challenge, &m) - (0..*m).fold(Scalar::zero(), |acc, j|{ acc + rho[j]*util::power(&challenge, &j)});
 
     let scalarstate: TriptychScalarState = TriptychScalarState {
         f, zA, zC, z
@@ -170,7 +156,8 @@ pub fn prove(M: &Vec<RistrettoPoint>, l: &usize, r: &Scalar, m: &usize) -> Signa
 
 }
 
-pub fn verify(M: &Vec<RistrettoPoint>, sgn: &Signature, m: &usize) -> Result<(), Errors> {
+// Verification of the base sigma protocol
+pub fn base_verify(M: &[RistrettoPoint], sgn: &Signature, m: &usize, message: &str) -> Result<(), Errors> {
     
     let mut transcript: Vec<u8> = Vec::with_capacity(1000);
     let ellipticState = &sgn.a;
@@ -186,6 +173,7 @@ pub fn verify(M: &Vec<RistrettoPoint>, sgn: &Signature, m: &usize) -> Result<(),
     for entry in M {
         transcript.extend_from_slice(entry.compress().as_bytes());
     }
+    transcript.extend_from_slice(message.as_bytes());
     transcript.extend_from_slice(ellipticState.J.compress().as_bytes());
     transcript.extend_from_slice(ellipticState.A.compress().as_bytes());
     transcript.extend_from_slice(ellipticState.B.compress().as_bytes());
@@ -230,7 +218,7 @@ pub fn verify(M: &Vec<RistrettoPoint>, sgn: &Signature, m: &usize) -> Result<(),
 
     let mut fourthRHSScalar = Scalar::zero();
     for k in 0..N {
-        let binary_k = pad(&k, &m); // This can be sped up by gray codes, will have to change after bench
+        let binary_k = util::pad(&k, &m); // This can be sped up by gray codes, will have to change after bench
         
         let mut product_term = Scalar::one();
 
@@ -251,4 +239,147 @@ pub fn verify(M: &Vec<RistrettoPoint>, sgn: &Signature, m: &usize) -> Result<(),
         return Err(TriptychError);
     }
     
+}
+
+pub fn KeyGen() -> (Scalar, RistrettoPoint) {
+    let mut rng = rand::thread_rng();
+    let r = Scalar::random(&mut rng);
+    let G = util::hash_to_point("G"); 
+
+    return (r, r*G);
+}
+
+// we need x to be secret key of one of the public keys in R for this ton work as of now
+// need to add error handling
+pub fn Sign(x: &Scalar, M: &str, R: &[RistrettoPoint]) -> Signature {
+    let G = util::hash_to_point("G"); 
+
+    let mut l: usize = 0;
+    for (i, element) in R.iter().enumerate() {
+        if  *element == x*G {
+            l = i;
+        }
+    }
+
+    let size = R.len();
+    let mut base = 1;
+    let mut m = 0;
+    while base < size {
+        base = base*2;
+        m = m+1;
+    }
+
+    return base_prove(R, &l, x, &m, M);
+}
+
+pub fn Verify(sgn: &Signature, M: &str, R: &[RistrettoPoint]) ->  Result<(), Errors> {
+    let size = R.len();
+    let mut base = 1;
+    let mut m = 0;
+
+    while base < size {
+        base = base*2;
+        m = m+1;
+    }
+
+    return base_verify(R, sgn, &m, M);
+}
+
+pub fn Link(sgn_a: &Signature, sgn_b: &Signature) -> bool {
+    return sgn_a.a.J == sgn_b.a.J;
+}
+
+#[cfg(test)]
+mod triptych_test {
+
+    use curve25519_dalek::ristretto::{RistrettoPoint};
+    use curve25519_dalek::scalar::Scalar;
+    use curve25519_dalek::traits::Identity;
+    use crate::signature::triptych;
+    use crate::util;
+    // use crate::Errors::{self, TriptychError};
+    // use std::convert::TryInto;
+    // use sha2::Sha512;
+    #[test]
+    pub fn test_power() {
+        let mut rng = rand::thread_rng();
+        let demo_scalar = Scalar::random(&mut rng);
+        let mut result = Scalar::one();
+        let exponent = 225;
+        for _ in 0..exponent{
+            result = result*demo_scalar;
+        }
+
+        assert_eq!(util::power(&demo_scalar, &exponent), result);
+    }
+
+    #[test]
+    pub fn test_pad(){
+        let result = util::pad(&127, &9);
+        let compare: Vec<usize> = vec![1, 1, 1, 1, 1, 1, 1, 0, 0];
+
+        assert_eq!(compare, result);
+    }
+
+    #[test]
+    pub fn test_convolve(){
+        let first = vec![Scalar::one(), Scalar::one()];
+        let second = vec![Scalar::one(), Scalar::one()];
+
+        assert_eq!(util::convolve(&first, &second), vec![Scalar::one(), Scalar::one() + Scalar::one(), Scalar::one()]);
+    }
+
+    #[test]
+    pub fn test_base_signature(){
+        let G = util::hash_to_point("G"); 
+        let m: usize = 4;
+        let l: usize = 12;
+        let len_M = 16;
+
+        let mut M: Vec<RistrettoPoint> = vec![RistrettoPoint::identity(); len_M];
+
+        let mut rng = rand::thread_rng();
+        let mut r: Scalar = Scalar::one();
+        for i in 0..len_M {
+            let sk = Scalar::random(&mut rng);
+            M[i] = sk*G;
+
+            if i == l {
+                r = sk;
+            }
+        }
+
+        let sgn: triptych::Signature = triptych::base_prove(&M, &l, &r, &m, "demo");
+
+        let result = triptych::base_verify(&M, &sgn, &m, "demo");
+
+        assert!(result.is_ok());
+
+    }
+
+    #[test]
+    pub fn test_signature(){
+        let size = 128;
+        let mut R: Vec<RistrettoPoint> = vec![RistrettoPoint::identity(); size];
+        let mut x: Scalar = Scalar::one();
+        let index = 14;
+
+        for i in 0..size {
+            let (sk, pk) = triptych::KeyGen();
+            R[i] = pk;
+
+            if i == index {
+                x = sk;
+            }
+        }
+        let M = "This is a triptych signature test, lets see if it works or not";
+
+        let sgn = triptych::Sign(&x, &M, &R);
+
+        let result = triptych::Verify(&sgn, &M, &R);
+
+        assert!(result.is_ok());
+
+    }
+
 }
--- libtriptych/src/util.rs
+++ libtriptych/src/util.rs
@@ -1,10 +1,28 @@
 use curve25519_dalek::ristretto::{RistrettoPoint};
 use curve25519_dalek::scalar::Scalar;
-
-
-
 use sha2::Sha512;
 
+pub fn pad(n: &usize, m: &usize) -> Vec<usize> {
+    let mut s = format!("{:b}", n); // this is using base 2, for other bases this will change
+    
+    if s.chars().count() < *m{
+        s = format!("{}{}", "0".repeat(*m - s.chars().count()), s);
+    }
+
+    return s.chars().rev().map(|i| i as usize - '0' as usize).collect::<Vec<usize>>();
+}
+
+pub fn convolve(x: &[Scalar], y: &[Scalar]) -> Vec<Scalar> {
+    let mut r: Vec<Scalar> = vec![Scalar::zero(); x.len()+1];
+
+    for i in 0..x.len(){
+        for j in 0..y.len(){
+            r[i+j] += x[i]*y[j];
+        }
+    }
+
+    return r;
+}
 
 pub fn hash_to_point(data: &str) -> RistrettoPoint {
     return RistrettoPoint::hash_from_bytes::<Sha512>(data.as_bytes());
@@ -35,16 +53,18 @@ pub fn delta(value: &usize, i: &usize) -> Scalar {
     }
 }
 
-pub fn power(base: &Scalar, exp: &usize) -> Scalar {
+pub fn power(base_: &Scalar, exp_: &usize) -> Scalar {
     let mut answer = Scalar::one();
-
-    while exp > &0 {
+    let mut exp = exp_.clone();
+    let mut base = base_.clone();
+    while exp > 0 {
         if exp % 2 == 1 {
-            let answer = answer*base;
+            answer = answer*base;
+            exp = exp-1;
         }
         else {
-            let exp = exp/2;
-            let base = base*base;
+            exp = exp/2;
+            base = base*base;
         }
 
     }
